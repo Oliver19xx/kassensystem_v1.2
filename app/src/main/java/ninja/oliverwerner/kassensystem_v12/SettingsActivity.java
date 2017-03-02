c87dbad0e80e170f657844d0e1fe0ee26cb23a98
Oliver19xx <oliver.werner94@gmail.com>
2017-03-02 23:18:20 +0100

    Einstellungen ergänzt - Layout geändert - Colorpicker eingebaut von QuadFlask
    Notiz:
    - Speichern des Farbcodes geht noch nicht wieder
    
    Quele: https://github.com/QuadFlask/colorpicker/

--- app/src/main/java/ninja/oliverwerner/kassensystem_v12/SettingsActivity.java
+++ app/src/main/java/ninja/oliverwerner/kassensystem_v12/SettingsActivity.java
@@ -1,10 +1,13 @@
 package ninja.oliverwerner.kassensystem_v12;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -17,9 +20,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorChangedListener;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
+import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static Context CONTEXT;
 
     // Festgelegte Feldname für die Speicherplätze in der Datei
     private static final String SHOP_NAME = "SHOP_NAME";
@@ -33,49 +48,119 @@ public class SettingsActivity extends AppCompatActivity
 
     // Felder aus dem Activity-Layout
     EditText etShopName;
-    EditText etThemeColor;
+    Button btThemeColor;
     Button btnSave;
+
+    // Für den Color-Picker
+    private int currentBackgroundColor = 0xffffffff;
+    private View root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CONTEXT = this.getBaseContext();
+
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Views des Layouts in Variablen laden
         etShopName = (EditText) findViewById(R.id.et_shop_name);
-        etThemeColor = (EditText) findViewById(R.id.et_theme_color);
+        btThemeColor = (Button) findViewById(R.id.et_theme_color);
         btnSave = (Button) findViewById(R.id.btn_save);
 
+        root = findViewById(R.id.color_screen);
+        changeBackgroundColor(currentBackgroundColor);
+        btThemeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Context context = SettingsActivity.this;
+
+                ColorPickerDialogBuilder
+                        .with(context)
+                        .setTitle(R.string.color_dialog_title)
+                        .initialColor(currentBackgroundColor)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorChangedListener(new OnColorChangedListener() {
+                            @Override
+                            public void onColorChanged(int selectedColor) {
+                                // Handle on color change
+                                Log.d("ColorPicker", "onColorChanged: 0x" + Integer.toHexString(selectedColor));
+                            }
+                        })
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                                toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                            }
+                        })
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                changeBackgroundColor(selectedColor);
+                                if (allColors != null) {
+                                    StringBuilder sb = null;
+
+                                    for (Integer color : allColors) {
+                                        if (color == null)
+                                            continue;
+                                        if (sb == null)
+                                            sb = new StringBuilder("Color List:");
+                                        sb.append("\r\n#" + Integer.toHexString(color).toUpperCase());
+                                    }
+
+                                    if (sb != null)
+                                        Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .showColorEdit(true)
+                        .setColorEditTextColor(ContextCompat.getColor(SettingsActivity.this, android.R.color.holo_blue_bright))
+                        .build()
+                        .show();
+            }
+        });
         // Beim Klick auf Speichern ...
         btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Hole den aktuellen Text aus den Textfeldern und löschen Leerzeichen vom Anfang und Ende
-                String shopName = etShopName.getText().toString().trim();
-                String themeColor = etThemeColor.getText().toString().trim();
+                                       @Override
+                                       public void onClick(View view) {
+                                           // Hole den aktuellen Text aus den Textfeldern und löschen Leerzeichen vom Anfang und Ende
+                                           String shopName = etShopName.getText().toString().trim();
+                                           String themeColor = btThemeColor.getText().toString().trim();
 
-                // Stellt die Datei zum Schreiben bereit
-                SharedPreferences sharedPrefs = getSharedPreferences(FILENAME, 0);
+                                           // Stellt die Datei zum Schreiben bereit
+                                           SharedPreferences sharedPrefs = getSharedPreferences(FILENAME, 0);
 
-                // Es wird ein "Bearbeiter" für die Datei erstellt
-                SharedPreferences.Editor editor = sharedPrefs.edit();
+                                           // Es wird ein "Bearbeiter" für die Datei erstellt
+                                           SharedPreferences.Editor editor = sharedPrefs.edit();
 
-                // Schreibe in die Datei an Feld x String y und speicher die Datei ab
-                editor.putString(SHOP_NAME, shopName).commit();
-                editor.putString(THEME_COLOR, themeColor).commit();
-            }
-        });
+                                           // Schreibe in die Datei an Feld x String y und speicher die Datei ab
+                                           editor.putString(SHOP_NAME, shopName).commit();
+                                           editor.putString(THEME_COLOR, themeColor).commit();
+                                       }
+                                   }
+
+        );
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(new View.OnClickListener()
+
+                               {
+                                   @Override
+                                   public void onClick(View view) {
+                                       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                               .setAction("Action", null).show();
+                                   }
+                               }
+
+        );
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -87,6 +172,15 @@ public class SettingsActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void changeBackgroundColor(int selectedColor) {
+        currentBackgroundColor = selectedColor;
+        root.setBackgroundColor(selectedColor);
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -94,8 +188,8 @@ public class SettingsActivity extends AppCompatActivity
         sharedPrefs = getSharedPreferences(FILENAME, 0);
 
         // Trage den Text aus den Speicherplätzen in die Textfelder ein
-        etShopName.setText(sharedPrefs.getString(SHOP_NAME,null));
-        etThemeColor.setText(sharedPrefs.getString(THEME_COLOR,null));
+        etShopName.setText(sharedPrefs.getString(SHOP_NAME, null));
+        btThemeColor.setText(sharedPrefs.getString(THEME_COLOR, null));
     }
 
     @Override
@@ -138,39 +232,39 @@ public class SettingsActivity extends AppCompatActivity
 
         switch (id) {
             case R.id.nav_dashboard: {
-                Log.d("myMessage","nav_dashboard");
-                Intent intent = new Intent(this,MainActivity.class);
+                Log.d("myMessage", "nav_dashboard");
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
             }
             case R.id.nav_tables: {
-                Log.d("myMessage","nav_tables");
-                Intent intent = new Intent(this,TablesActivity.class);
+                Log.d("myMessage", "nav_tables");
+                Intent intent = new Intent(this, TablesActivity.class);
                 startActivity(intent);
                 break;
             }
             case R.id.nav_products: {
-                Log.d("myMessage","nav_products");
-                Intent intent = new Intent(this,ProductGroupsActivity.class);
-                intent.putExtra("orderId","");
+                Log.d("myMessage", "nav_products");
+                Intent intent = new Intent(this, ProductGroupsActivity.class);
+                intent.putExtra("orderId", "");
                 startActivity(intent);
                 break;
             }
             case R.id.nav_settings: {
-                Log.d("myMessage","nav_settings");
-                Intent intent = new Intent(this,SettingsActivity.class);
+                Log.d("myMessage", "nav_settings");
+                Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 break;
             }
             case R.id.nav_logout: {
-                Log.d("myMessage","nav_logout");
+                Log.d("myMessage", "nav_logout");
 //                Intent intent = new Intent(this,MainActivity.class);
 //                startActivity(intent);
                 setTitle("Logout");
                 break;
             }
             default: {
-                Log.d("myMessage","nav_default");
+                Log.d("myMessage", "nav_default");
 
             }
         }
